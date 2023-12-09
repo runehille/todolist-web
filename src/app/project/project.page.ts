4d270ef80863fa589993f2c9739118abb39efedd
runehille <rune.hille@gmail.com>
2023-12-09 17:38:31 +0100

    Refactor styles and dependencies

---
+++ src/app/project/project.page.ts
@@ -0,0 +1,13 @@
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  template: 'ProjectPage works!',
+  styles: [''],
+})
+export class ProjectPage {}
+
+@NgModule({
+  declarations: [ProjectPage],
+})
+export class ProjectPageModule {}
---
+++ src/app/shared/ui/top-bar.component.ts
@@ -0,0 +1,107 @@
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-top-bar',
+  template: `
+    <nav
+      class="container mx-auto p-3 px-6 min-w-full bg-white border-b-2 border-slate-200"
+    >
+      <div class="flex items-center justify-between">
+        <div class="flex items-center justify-between">
+          <div class="space-x-6 font-semibold">
+            <a class="text-4xl font-bold text-slate-800" href="#"> Todolist </a>
+            <a href="#" class="hover:text-slate-400 hover:border-b-2"
+              >Your work</a
+            >
+            <a href="#" class="hover:text-slate-400">Projects</a>
+            <a href="#" class="hover:text-slate-400">Filters</a>
+            <a href="#" class="hover:text-slate-400">Dashboards</a>
+            <a href="#" class="hover:text-slate-400">People</a>
+
+            <button
+              class="p-2 px-4 text-white bg-slate-900 rounded-md hover:bg-slate-800"
+            >
+              Create
+            </button>
+          </div>
+        </div>
+        <div class="flex items-center space-x-3 justify-between">
+          <input
+            type="text"
+            placeholder="search"
+            [(ngModel)]="searchvalue"
+            class="border-2 border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:border-blue-400"
+          />
+          <a href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+          <a href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 01-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 01-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 01-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584zM12 18a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+          <a href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="https://picsum.photos/35" alt="" class="rounded-full" />
+          </a>
+        </div>
+      </div>
+    </nav>
+  `,
+  styles: [``],
+})
+export class TopBarComponent {
+  searchvalue: string = '';
+}
+
+@NgModule({
+  declarations: [TopBarComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [TopBarComponent],
+})
+export class TopBarModule {}
